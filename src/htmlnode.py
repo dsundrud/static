7705940a32457fccd38f2f92a670649c89cdf277
your_github_usernamedsundrud <dsundrud@gmail.com>
2025-03-06 14:11:07 -0500

    finished nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,4 @@
+from textnode import TextType, TextNode
 class HTMLNode:
     def __init__(self, tag = None, value = None, children = None, props= None):
         self.tag = tag
@@ -74,6 +75,22 @@ class ParentNode(HTMLNode):
         
         return html
     
+    def text_node_to_html_node(text_node):
+        if text_node.text_type == TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.text) 
+        elif text_node.text_type == TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text)
+        elif text_node.text_type == TextType.ITALIC:
+            return LeafNode(tag="i", value=text_node.text)
+        elif text_node.text_type == TextType.CODE:
+            return LeafNode(tag="code", value=text_node.text)
+        elif text_node.text_type == TextType.LINK:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        elif text_node.text_type == TextType.IMAGE:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+        else:
+            raise Exception("Unacceptable Type")
+
 
     def test_to_html_with_children(self):
         child_node = LeafNode("span", "child")
@@ -84,7 +101,10 @@ class ParentNode(HTMLNode):
         grandchild_node = LeafNode("b", "grandchild")
         child_node = ParentNode("span", [grandchild_node])
         parent_node = ParentNode("div", [child_node])
-        self.assertEqual(
-            parent_node.to_html(),
-            "<div><span><b>grandchild</b></span></div>",
-    )
+        self.assertEqual(parent_node.to_html(),"<div><span><b>grandchild</b></span></div>")
+        
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
